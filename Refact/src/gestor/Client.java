18f29223fc925eafc86cdf9b12d344a540c8916b
jhoelpacheco <jhoel.pacheco12@gmail.com>
2021-04-23 16:50:10 +0200

    segundo commit

--- Refact/src/gestor/Client.java
+++ Refact/src/gestor/Client.java
@@ -45,24 +45,7 @@ public class Client {
             getNom() +
             " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-            double quantitat = 0;
-            switch (lloguer.getVehicle().getCategoria()) {
-                case Vehicle.BASIC:
-                    quantitat += 3;
-                    if (lloguer.getDies() > 3) {
-                        quantitat += (lloguer.getDies() - 3) * 1.5;
-                    }
-                    break;
-                case Vehicle.GENERAL:
-                    quantitat += 4;
-                    if (lloguer.getDies() > 2) {
-                        quantitat += (lloguer.getDies() - 2) * 2.5;
-                    }
-                    break;
-                case Vehicle.LUXE:
-                    quantitat += lloguer.getDies() * 6;
-                    break;
-            }
+            double quantitat = quantitatPerLloguer(lloguer);
 
             // afegeix lloguers freqüents
             bonificacions ++;
@@ -86,5 +69,26 @@ public class Client {
         resultat += "Import a pagar: " + total + "€\n" +
             "Punts guanyats: " + bonificacions + "\n";
         return resultat;
+    }
+    public double quantitatPerLloguer(Lloguer lloguer){
+    	double quantitat = 0;
+        switch (lloguer.getVehicle().getCategoria()) {
+            case Vehicle.BASIC:
+                quantitat += 3;
+                if (lloguer.getDies() > 3) {
+                    quantitat += (lloguer.getDies() - 3) * 1.5;
+                }
+                break;
+            case Vehicle.GENERAL:
+                quantitat += 4;
+                if (lloguer.getDies() > 2) {
+                    quantitat += (lloguer.getDies() - 2) * 2.5;
+                }
+                break;
+            case Vehicle.LUXE:
+                quantitat += lloguer.getDies() * 6;
+                break;
+        }
+        return quantitat;
     }
 }
