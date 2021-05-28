33b331ba2295e1a9734e0c9f3e11eecc665d55a2
Jhoel <Jhoel@DESKTOP-85PSMPE>
2021-05-28 16:25:45 +0100

    Cuarto commit

--- Refact/src/gestor/Client.java
+++ Refact/src/gestor/Client.java
@@ -37,7 +37,6 @@ public class Client {
     public boolean conte(Lloguer lloguer) {
         return lloguers.contains(lloguer);
     }
-
     public String informe() {
         double total = 0;
         int bonificacions = 0;
@@ -45,52 +44,26 @@ public class Client {
             getNom() +
             " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-            //double quantitat = quantitatPerLloguer(lloguer);
-            double quantitat = lloguer.quantitat();
-            
-            
-            // afegeix lloguers freqüents
-            bonificacions ++;
-
-            // afegeix bonificació per dos dies de lloguer de Luxe
-            if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
-                    lloguer.getDies()>1 ) {
-                bonificacions ++;
-            }
-
-            // composa els resultats d'aquest lloguer
+            bonificacions += bonificacionsDeLloguer(lloguer);
+        	
             resultat += "\t" +
                 lloguer.getVehicle().getMarca() +
                 " " +
                 lloguer.getVehicle().getModel() + ": " +
-                (quantitat * 30) + "€" + "\n";
-            total += quantitat * 30;
+                (lloguer.quantitat() * 30) + "€" + "\n";
+            total += lloguer.quantitat() * 30;
         }
-
-        // afegeix informació final
         resultat += "Import a pagar: " + total + "€\n" +
             "Punts guanyats: " + bonificacions + "\n";
         return resultat;
     }
-    /*public double quantitatPerLloguer(Lloguer lloguer){
-    	double quantitat = 0;
-        switch (lloguer.getVehicle().getCategoria()) {
-            case Vehicle.BASIC:
-                quantitat += 3;
-                if (lloguer.getDies() > 3) {
-                    quantitat += (lloguer.getDies() - 3) * 1.5;
-                }
-                break;
-            case Vehicle.GENERAL:
-                quantitat += 4;
-                if (lloguer.getDies() > 2) {
-                    quantitat += (lloguer.getDies() - 2) * 2.5;
-                }
-                break;
-            case Vehicle.LUXE:
-                quantitat += lloguer.getDies() * 6;
-                break;
-        }
-        return quantitat;
-    }*/
+	public int bonificacionsDeLloguer(Lloguer lloguer) {
+		int bonificacions = 0;
+		if(lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
+				lloguer.getDies()>1) {
+			bonificacions++;
+		}
+		return bonificacions;
+	}
+    
 }
